a960c69355c6ca2e464fca803e65a6baee8e4e38
Jannis Reis <jannis.reis@swissonline.ch>
2017-05-03 14:35:07 +0200

    test listview

--- VirtualTimetable/Timetable.Designer.cs
+++ VirtualTimetable/Timetable.Designer.cs
@@ -35,6 +35,7 @@
             this.cBoxFrom = new System.Windows.Forms.ComboBox();
             this.cBoxTo = new System.Windows.Forms.ComboBox();
             this.btnSearch = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listResults
@@ -103,11 +104,21 @@
             this.btnSearch.UseVisualStyleBackColor = true;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Search2";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Timetable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.cBoxTo);
             this.Controls.Add(this.cBoxFrom);
@@ -132,5 +143,6 @@
         private System.Windows.Forms.ComboBox cBoxFrom;
         private System.Windows.Forms.ComboBox cBoxTo;
         private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button button1;
     }
 }
